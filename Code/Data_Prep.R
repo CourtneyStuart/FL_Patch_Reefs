3ee911c0811dd41a6f5fa779dc0806c703f9bcc4
Courtney Stuart <courtney.e.stuart@gmail.com>
2022-07-17 18:33:32 -0600

    Updated correlation check to include predator density predictor

--- Code/Data_Prep.R
+++ Code/Data_Prep.R
@@ -3,27 +3,24 @@
 # set working directory (home project folder)
 setwd("E:/BIOL398_Patch_Reef_Residency/")
 
-# associated github repo: 
-# https://github.com/CourtneyStuart/FL_Patch_Reefs
-
 # data directories
 # joined reef fish (abundance, presence/absence, etc.) and patch reef (area,
 # perimeter, P:A ratio, distance to reef/mangrove, etc.) data exported from
 # ArcGIS
 data_wd = "E:/BIOL398_Patch_Reef_Residency/Tabular_Data/"
 
-# for saving data to github
-git_data = "E:/BIOL398_Patch_Reef_Residency/GitHub/FL_Patch_Reefs/Data/"
-
-# for saving figures to github
-git_figs = "E:/BIOL398_Patch_Reef_Residency/GitHub/FL_Patch_Reefs/Figures/"
-
 # temporary directory (for intermediate/temporary products or checks)
 temp_wd = "E:/BIOL398_Patch_Reef_Residency/Temporary/"
 
+# saving figures
+fig_wd = "E:/BIOL398_Patch_Reef_Residency/Figures/"
+
+# github repo project
+git_wd = "E:/BIOL398_Patch_Reef_Residency/GitHub/FL_Patch_Reefs/"
+
 # install packages
 #install.packages(c("conflicted", "tidyverse", "ggplot2", "PNWColors", "readxl",
-# "dplyr", "corrplot", "Cairo", "usdm", "ISLR"))
+#"dplyr", "corrplot", "Cairo", "usdm", "ISLR"))
 
 # load packages
 library(easypackages) # lets you load more than one package at once
@@ -40,13 +37,13 @@ set.seed(123)
 # (depth & seafloor morphometrics, seasonal water conditions, surrounding
 # seascape context (i.e., area of seagrass, reef, and unconsolidated sediments
 # in 500 m buffer around survey site)), and corresponding patch reef data 
-# (patch area, perimeter, perimeter:area, nearest neighbor distance)
+# (patch area, perimeter, perimeter:area, nearest neighbor distance, predators)
 
 
 #### SUBADULT GRAY SNAPPER (Lutjanus griseus) ####
-# reading in the excel data sheet exported from ArcGIS
-lg = as.data.frame(read_xls(
-  path = paste0(data_wd, "Subadult_Gray_Snapper_Patch_Reef_Data.xls")))
+# reading in the csv file containing gray snapper observations with patch reef 
+# biogeophysical predictors
+lg = read.csv(paste0(data_wd, "Subadult_Gray_Snapper_Patch_Reefs_w_Predator.csv"))
 
 # str lets you preview data and column types/formats
 str(lg)
@@ -56,7 +53,7 @@ str(lg)
 #View(lg)
 
 # save the raw data to github so we have a safe copy there
-write.csv(lg, paste0(git_data, "Subadult_Gray_Snapper_Patch_Reef_Data.csv"),
+write.csv(lg, paste0(git_wd, "Data/Subadult_Gray_Snapper_Patch_Reef_Data.csv"),
           row.names = FALSE)
 
 # first, let's do some data prep and cleaning to make the data frame easier
@@ -75,20 +72,27 @@ lg = lg %>%
          Patch_Neigh_Dist = NEAR_DIST, # for each patch reef, distance to nearest neighbor patch reef (meters)
          Area_CRHB = Coral_Reef_and_Hardbottom, # coral reef and hard bottom area in 500 m buffer (square meters)
          Area_SG = Seagrass, # seagrass area in 500 m buffer (square meters)
-         Area_US = Unconsolidated_Sediment) %>%  # unconsolidated sediment area in 500 m buffer (square meters)
-  # let's make sure that our two presence/absence columns are stored as factors
+         Area_US = Unconsolidated_Sediment, # unconsolidated sediment area in 500 m buffer (square meters)
+         Pred_Abundance = N_PRED, # predator abundance
+         Pred_Density = DEN_PRED) %>%  # predator density
+  # let's make sure that our two response presence/absence columns are factors
   mutate(Presence = as.factor(Presence),
          Presence2 = as.factor(Presence2)) %>%
-  # add new column (col 58) storing the area sampled by each reef fish survey,
-  # where area = pie*radius^2 --> pie*7.5m^2 --> ~176.71 m^2
-  mutate(Survey_Area = rep(as.numeric(176.71), nrow(.))) %>%
-  # add new column (col 59) storing the density of sub-adult gray snapper,
+  # add new column storing the area sampled by each reef fish survey,
+  # where area = pie*radius^2 --> pie*7.5m^2 --> ~177 m^2
+  mutate(Survey_Area = rep(as.numeric(177), nrow(.))) %>%
+  # add new column storing the density of sub-adult gray snapper,
   # where density = abundance/area of sampling = fish per m^2
   mutate(Density = Abundance/Survey_Area) %>%
-  # now keep only the columns we need (selecting by column # rather than name 
-  # because it's easier to write), the order here will determine column order
+  # now keep only the columns we need, the order here will determine column order
   # in the output dataframe 
-  select(5:9, 59, 58, 11:12, 52, 3, 14, 57, 15:28, 47:49, 51, 53, 55:56)
+  select(Patch_ID, Species, Life_Stage, Presence, Presence2, Abundance, Density, 
+         Survey_Area, Long_M, Lat_M, Habitat, Mangrove_Dist, Reef_Dist,
+         Depth, StDev_Depth, Slope, Curvature, Plan_Curve, BPI_Fine, 
+         BPI_Broad, Rugosity, Mean_Sum_Temp, Mean_Sum_DO, Mean_Sum_Sal, 
+         Mean_Win_Temp, Mean_Win_DO, Mean_Win_Sal, Patch_Area,
+         Patch_Perimeter, Patch_Neigh_Dist, Area_CRHB, Area_SG, Area_US, 
+         Pred_Abundance, Pred_Density)
 
 # sort data by increasing latitude and then add a NEW object_id column based on 
 # this sorting (later, we will repeat this for bluestriped grunt and use the 
@@ -100,7 +104,7 @@ lg$Object_ID = seq.int(nrow(lg))
 # environmental conditions at patch reefs that had multiple surveys around them)
 lg = lg %>% 
   group_by(Patch_ID) %>% 
-  sample_n(1) %>% # select one record at random per patch reef
+  sample_n(1) %>%
   ungroup()
 lg = as.data.frame(lg)
 
@@ -138,7 +142,7 @@ hist(lg$BPI_Broad)
 # fine-scale BPI was evaluated using concentric rings of 5 m and 125 m
 hist(lg$BPI_Fine)
 
-# based on my first MSc chapter, which modeled habitat suitability for sub-adult
+# based on my first chapter, which modeled habitat suitability for sub-adult
 # gray snapper and bluestriped grunt across the seascape, broad-scale BPI was a 
 # much stronger predictor of suitability than fine-scale BPI. also, as we can see
 # in these histograms, fine-scale BPI takes on only a few values when examined
@@ -158,11 +162,13 @@ hist(lg$BPI_Fine)
 # variables that we'd like to consider for modeling
 lg_vars = lg %>%
   # where (-) lets you deselect a column
-  select(12:34, -Curvature, -Plan_Curve, -Rugosity, -BPI_Fine,
-         -Mean_Sum_DO, -Mean_Win_DO)
+  select(11:35, -Curvature, -Plan_Curve, -Rugosity, -BPI_Fine,
+         -Mean_Sum_DO, -Mean_Win_DO, -Pred_Abundance) # we'll focus on density
 
 # let's check to make sure all the variable columns are of the numeric type
 str(lg_vars)
+lg_vars = as.data.frame(lapply(lg_vars, as.numeric))
+
 
 # now we can check for correlation among these variables, where strong
 # positive or negative correlation (> absolute 0.7) suggests that the two
@@ -171,7 +177,7 @@ str(lg_vars)
 # collinearity (or multicollinearity, when more than two variables are 
 # highly correlated) can distort model interpretations, coefficient 
 # estimates, and levels of significance. as a rule of thumb, we'll use
-# pearson pairwise correlation (r) threshold of |0.7|. 
+# pearson pairwise correlation (r) threshold of |0.7| (abs). 
 
 # first we'll create a full correlation matrix from all variables
 lg_cormat = cor(lg_vars, use = "complete.obs")
@@ -180,7 +186,7 @@ lg_cormat = cor(lg_vars, use = "complete.obs")
 palette = pnw_palette("Shuksan2", 200, type = "continuous")
 
 # set plotting margins
-par(mar = c(0,0,0,0))
+par(mar=c(0,0,0,0))
 
 # full correlation plot
 corrplot(lg_cormat, method = "color", col = palette, type = "upper",
@@ -188,7 +194,8 @@ corrplot(lg_cormat, method = "color", col = palette, type = "upper",
          number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.8)
 
 # save full correlation plot as a png
-Cairo(file = paste0(git_figs, "Correlation_Full_Predictor_Set.png"), 
+# local folder
+Cairo(file = paste0(fig_wd, "Correlation_Full_Predictor_Set.png"), 
       bg = "white", type = "png", units = "in", width = 6, 
       height = 6, pointsize = 12, dpi = 300)
 par(mar=c(0,0,0,0))
@@ -196,6 +203,17 @@ corrplot(lg_cormat, method = "color", col = palette, type = "upper",
          order = "original", addCoef.col = "black", number.cex = 0.50, 
          number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.75)
 dev.off()
+
+# github repo
+Cairo(file = paste0(git_wd, "Figures/Correlation_Full_Predictor_Set.png"), 
+      bg = "white", type = "png", units = "in", width = 6, 
+      height = 6, pointsize = 12, dpi = 300)
+par(mar=c(0,0,0,0))
+corrplot(lg_cormat, method = "color", col = palette, type = "upper",
+         order = "original", addCoef.col = "black", number.cex = 0.50, 
+         number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.75)
+dev.off()
+
 
 # we could run through the correlation matrix by hand to find problematic
 # variables...but instead we'll automate the process using a "usdm" function. 
@@ -209,7 +227,7 @@ lg_corr # check correlation results
 # VIF measures how much the behavior (variance) of a variable is influenced
 # by it's interaction with other variables. VIF allows a quick measure of how
 # much a variable is contributing to the standard error in the regression. we
-# want to keep standard errors as small as possible, so we will use a 
+# want to keep standard errors as small as possible, so we will use a standard
 # VIF threshold of 5.
 
 # the vifstep function runs through all pairs of variables in the lg_vars
@@ -225,18 +243,18 @@ cor(lg_vars$StDev_Depth, lg_vars$Slope,  use = "complete")
 # yes, they're extremely positively correlated. In this case, we will retain
 # slope as suggested.
 
-# what about the suggestion to remove patch perimeter? this is likely due to 
-# correlation between patch perimeter and patch area...
-cor(lg_vars$Patch_Perimeter, lg_vars$Patch_Area)
-# once again, these variables display strong positive correlation. In this case,
-# patch area may be a more ecologically relevant predictor because we assume that 
-# reef fish are more likely to respond to the overall area of a patch reef compared
-# to its perimeter. so we will retain patch area and remove patch perimeter in 
-# the next step, hopefully this will resolve the issue.
+# what about the suggestion to remove patch area? this is likely due to 
+# correlation between patch area and patch perimeter...
+cor(lg_vars$Patch_Area, lg_vars$Patch_Perimeter)
+# once again, these variables display strong positive correlation. However,
+# in this case, patch area may be a more ecologically relevant predictor.
+# this is because we assume that reef fish are more likely to respond to
+# patch area than perimeter. so we will retain patch area and remove patch 
+# perimeter in the next step, hopefully this will resolve the issue.
 
 # finally, what about seagrass area within a 500 m buffer? this is possibly 
 # correlated with either area of coral & hardbottom or unconsolidated sediment
-cor(lg_vars$Area_SG, lg_vars$Area_CRHB) # below our |0.7| threshold
+cor(lg_vars$Area_SG, lg_vars$Area_CRHB) # below our threshold
 cor(lg_vars$Area_SG, lg_vars$Area_US) # strong negative correlation
 # here, area of surrounding seagrass is what we're more so interested in, 
 # because this can serve as a proxy for available prey (where gray snapper
@@ -255,7 +273,8 @@ corrplot(lg_cormat2, method = "color", col = palette, type = "upper",
          number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.8)
 
 # save reduced correlation plot as a png
-Cairo(file = paste0(git_figs, "Correlation_Reduced_Predictor_Set.png"), 
+# local folder
+Cairo(file = paste0(fig_wd, "Correlation_Reduced_Predictor_Set.png"), 
       bg = "white", type = "png", units = "in", width = 6, 
       height = 6, pointsize = 12, dpi = 300)
 par(mar=c(0,0,0,0))
@@ -264,7 +283,17 @@ corrplot(lg_cormat2, method = "color", col = palette, type = "upper",
          number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.75)
 dev.off()
 
-# check correlation again just to make sure there are no other issues
+# github repo
+Cairo(file = paste0(git_wd, "Figures/Correlation_Reduced_Predictor_Set.png"), 
+      bg = "white", type = "png", units = "in", width = 6, 
+      height = 6, pointsize = 12, dpi = 300)
+par(mar=c(0,0,0,0))
+corrplot(lg_cormat2, method = "color", col = palette, type = "upper",
+         order = "original", addCoef.col = "black", number.cex = 0.50, 
+         number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.75)
+dev.off()
+
+# check correlation again
 lg_corr2 = vifcor(lg_vars2, th = 0.7)
 lg_corr2
 
@@ -275,12 +304,12 @@ lg_vif2
 # reset ploting margins 
 par(mar=c(2.5, 2.5, 2.5, 2.5))
 
-# no outstanding collinearity problems! let's create and save some
+# no outstanding multicollinearity issues! let's create and save some
 # data frames with these variables. first, a data frame with all rows.
 lg_full = lg %>%
   select(-Curvature, -Plan_Curve, -Rugosity, -BPI_Fine,
          -Mean_Sum_DO, -Mean_Win_DO, -StDev_Depth,
-         -Patch_Perimeter, -Area_US)
+         -Patch_Perimeter, -Area_US, -Pred_Abundance)
 
 # randomize the order of rows
 lg_random = sample(nrow(lg_full))
@@ -289,20 +318,28 @@ lg_full = lg_full[lg_random, ]
 # now randomly split data into two subsets: one for model calibration (70%)
 # and one for model validation (30%)
 lg_train_id = sample(seq_len(nrow(lg_full)), size = floor(0.70*nrow(lg_full)))  
-lg_train = lg_full[lg_train_id,] # creates the training subset (70%)
-lg_test = lg_full[-lg_train_id,]  # creates the testing subset (30%)
+lg_train = lg_full[lg_train_id,] # creates the training dataset (70%)
+lg_test = lg_full[-lg_train_id,]  # creates the test dataset (30%)
 
-# let's save these data frames as csv files in our github repo
-write.csv(lg_train, paste0(git_data, "Subadult_Gray_Snapper_Model_Calibration_Data.csv"),
+# let's save these data frames as csv files 
+# local folders
+write.csv(lg_train, paste0(data_wd, "Subadult_Gray_Snapper_Model_Calibration_Data.csv"),
           row.names = FALSE)
 
-write.csv(lg_test, paste0(git_data, "Subadult_Gray_Snapper_Model_Validation_Data.csv"),
+write.csv(lg_test, paste0(data_wd, "Subadult_Gray_Snapper_Model_Validation_Data.csv"),
+          row.names = FALSE)
+
+# github repo
+write.csv(lg_train, paste0(git_wd, "Data/Subadult_Gray_Snapper_Model_Calibration_Data.csv"),
+          row.names = FALSE)
+
+write.csv(lg_test, paste0(git_wd, "Data/Subadult_Gray_Snapper_Model_Validation_Data.csv"),
           row.names = FALSE)
 
 #### SUBADULT BLUESTRIPED GRUNT (Haemulon sciurus) ####
-# reading in the excel data sheet exported from ArcGIS
-hs = as.data.frame(read_xls(
-  path = paste0(data_wd, "Subadult_Bluestriped_Grunt_Patch_Reef_Data.xls")))
+# reading in the csv file containing bluestriped grunt observations with patch reef 
+# biogeophysical predictors
+hs = read.csv(paste0(data_wd, "Subadult_Bluestriped_Grunt_Patch_Reefs_w_Predator.csv"))
 
 # str lets you preview data and column types/formats
 str(hs)
@@ -312,7 +349,7 @@ str(hs)
 #View(hs)
 
 # save the raw data to github so we have a safe copy there
-write.csv(hs, paste0(git_data, "Subadult_Bluestriped_Grunt_Patch_Reef_Data.csv"),
+write.csv(hs, paste0(git_wd, "Data/Subadult_Bluestriped_Grunt_Patch_Reef_Data.csv"),
           row.names = FALSE)
 
 # first, let's do some data prep and cleaning to make the data frame easier
@@ -331,23 +368,30 @@ hs = hs %>%
          Patch_Neigh_Dist = NEAR_DIST, # for each patch reef, distance to nearest neighbor patch reef (meters)
          Area_CRHB = Coral_Reef_and_Hardbottom, # coral reef and hard bottom area in 500 m buffer (square meters)
          Area_SG = Seagrass, # seagrass area in 500 m buffer (square meters)
-         Area_US = Unconsolidated_Sediment) %>%  # unconsolidated sediment area in 500 m buffer (square meters)
-  # let's make sure that our two presence/absence columns are stored as factors
+         Area_US = Unconsolidated_Sediment, # unconsolidated sediment area in 500 m buffer (square meters)
+         Pred_Abundance = N_PRED, # predator abundance
+         Pred_Density = DEN_PRED) %>%  # predator density
+  # let's make sure that our two response presence/absence columns are factors
   mutate(Presence = as.factor(Presence),
          Presence2 = as.factor(Presence2)) %>%
-  # add new column (col 58) storing the area sampled by each reef fish survey,
-  # where area = pie*radius^2 --> pie*7.5m^2 --> ~176.71 m^2
-  mutate(Survey_Area = rep(as.numeric(176.71), nrow(.))) %>%
-  # add new column (col 59) storing the density of sub-adult gray snapper,
+  # add new column storing the area sampled by each reef fish survey,
+  # where area = pie*radius^2 --> pie*7.5m^2 --> ~177 m^2
+  mutate(Survey_Area = rep(as.numeric(177), nrow(.))) %>%
+  # add new column storing the density of sub-adult bluestriped grunt,
   # where density = abundance/area of sampling = fish per m^2
   mutate(Density = Abundance/Survey_Area) %>%
-  # now keep only the columns we need (selecting by column # rather than name 
-  # because it's easier to write), the order here will determine column order
+  # now keep only the columns we need, the order here will determine column order
   # in the output dataframe 
-  select(5:9, 59, 58, 11:12, 52, 3, 14, 57, 15:28, 47:49, 51, 53, 55:56)
+  select(Patch_ID, Species, Life_Stage, Presence, Presence2, Abundance, Density, 
+         Survey_Area, Long_M, Lat_M, Habitat, Mangrove_Dist, Reef_Dist,
+         Depth, StDev_Depth, Slope, Curvature, Plan_Curve, BPI_Fine, 
+         BPI_Broad, Rugosity, Mean_Sum_Temp, Mean_Sum_DO, Mean_Sum_Sal, 
+         Mean_Win_Temp, Mean_Win_DO, Mean_Win_Sal, Patch_Area,
+         Patch_Perimeter, Patch_Neigh_Dist, Area_CRHB, Area_SG, Area_US, 
+         Pred_Abundance, Pred_Density)
 
-# we DO NOT need to repeat the correlation assessment because the gray 
-# snapper and bluestriped grunt data came from the SAME EXACT surveys & sites.
+# we DO NOT have to repeat the correlation assessment because all gray 
+# snapper and bluestriped grunt data come from the SAME EXACT surveys.
 # so if we were to repeat the correlation and VIF tests, we would get 
 # identical results! instead, jump ahead to saving the modeling datasets:
 # full, calibration, and validation. 
@@ -361,7 +405,7 @@ hs$Object_ID = seq.int(nrow(hs))
 hs_full = hs %>%
   select(-Curvature, -Plan_Curve, -Rugosity, -BPI_Fine,
          -Mean_Sum_DO, -Mean_Win_DO, -StDev_Depth,
-         -Patch_Perimeter, -Area_US)
+         -Patch_Perimeter, -Area_US, -Pred_Abundance)
 
 # now, use the object_id values stored in the gray snapper training and testing
 # dataframes to extract bluestriped grunt data from the same exact locations
@@ -371,9 +415,17 @@ hs_train = hs_full %>%
 hs_test = hs_full %>%
   filter(Object_ID %in% lg_test$Object_ID)
 
-# save these data as csv files in our github repo for modeling
-write.csv(hs_train, paste0(git_data, "Subadult_Bluestriped_Grunt_Model_Calibration_Data.csv"),
+# save these data as csv files for modeling
+# local folders
+write.csv(hs_train, paste0(data_wd, "Subadult_Bluestriped_Grunt_Model_Calibration_Data.csv"),
           row.names = FALSE)
 
-write.csv(hs_test, paste0(git_data, "Subadult_Bluestriped_Grunt_Model_Validation_Data.csv"),
+write.csv(hs_test, paste0(data_wd, "Subadult_Bluestriped_Grunt_Model_Validation_Data.csv"),
+          row.names = FALSE)
+
+# github repo
+write.csv(hs_train, paste0(git_wd, "Data/Subadult_Bluestriped_Grunt_Model_Calibration_Data.csv"),
+          row.names = FALSE)
+
+write.csv(hs_test, paste0(git_wd, "Data/Subadult_Bluestriped_Grunt_Model_Validation_Data.csv"),
           row.names = FALSE)
